077278e0a4f44b7778aca08e62c1d58dec36e20e
Charles Ivia <iviacharles@gmail.com>
2020-09-25 12:18:24 +0300

    Working with associative collections

--- python_fundamentals/collections.py
+++ python_fundamentals/collections.py
@@ -4,7 +4,7 @@ x = [2, 9.1, 12.5]
 
 mean = np.mean(x)
 
-print(np.mean(x) == sum(x)/len(x))
+print(np.mean(x) == sum(x) / len(x))
 
 print(mean)
 
@@ -28,7 +28,7 @@ first, second, third = b
 
 print(first, "\n", second, "\n", third)
 
-#tuple vs list
+# tuple vs list
 
 china_data = [(2015, 11.06, 1.371), (2014, 10.48, 1.364), (2013, 9.607, 1.357)]
 
@@ -36,9 +36,9 @@ print(china_data)
 
 print(china_data[0][0])
 
-#ZIP AND ENUMERATE
+# ZIP AND ENUMERATE
 
-#zip
+# zip
 
 gdp_data = [9.607, 10.48, 11.06]
 years = [2013, 2014, 2015]
@@ -55,8 +55,124 @@ print(x, y)
 print(f"year = {x}, GDP = {y}")
 
 
-#enumerate
+# enumerate
 
 e = enumerate(["a", "b", "c"])
 lst = list(e)
 print(lst)
+
+
+# List methods .extend(), .sort(), .append()
+
+
+# ASSOCIATIVE COLLECTIONS
+
+# dictionaries/dict
+
+china_data = {"country": "China", "year": 2015, "GDP": 11.06, "population": 1.371}
+
+companies = {
+    "AAPL": {"bid": 175.96, "ask": 175.98},
+    "GE": {"bid": 1047.03, "ask": 1048.40},
+    "TVIX": {"bid": 8.38, "ask": 8.40},
+}
+
+print(companies)
+
+# getting, setting and updating dict items
+
+print(china_data["year"])
+
+print(f"country = {china_data['country']}, population = {china_data['population']}")
+
+china_data["unemployment"] = "4.05%"
+
+print(china_data)
+
+china_data["unemployment"] = 4.05
+
+print(china_data)
+
+# number of key calue pair in a dict
+
+print(len(china_data))
+
+# get a list of all the keys
+
+ls = list(china_data.keys())
+
+print(ls)
+
+# get a list of all the values
+
+lsv = list(china_data.values())
+print(lsv)
+
+
+# Overwrite and add key-value pairs
+
+more_china_data = {
+    "irrigated_land": 690_070,
+    "top_religions": {"buddhist": 18.2, "christian": 5.1, "muslim": 1.8},
+}
+
+
+china_data.update(more_china_data)
+print(china_data)
+
+# Get the value associated with a key or return a default value
+# use this to avoid the NameError
+
+print(china_data.get("irrigated_land", "Data Not Available"))
+print(china_data.get("death_rate", "Data Not Available"))
+
+# Sets
+
+
+s = {1, "hello", 3.0}
+print(type(s), s)
+
+
+print("hello" in s)
+
+s.add(100)
+print(s)
+
+s.add("hello")  # does not work as sets have unique items
+print(s)
+
+s2 = {"hello", 5, 6, "there"}
+
+print(s.union(s2))  # returns a set with all elements in either s or s2
+
+print(s.intersection(s2))  # returns a set with all elements in both s and s2
+
+print(s.difference(s2))  # returns a set with all elements in s that aren’t in s2
+
+print(
+    s.symmetric_difference(s2)
+)  # returns a set with all elements in only one of s and s2
+
+# converting lists and tuples to sets and vv
+
+x = [1, 2, 3, 1]
+print(set(x))
+
+t = (1, 2, 3, 1)
+print(set(t))
+
+print(list(s))
+print(tuple(s))
+
+
+# exercise
+
+
+lst = enumerate(("foo"))
+print(list(lst))
+
+
+china_data.pop("irrigated_land")
+china_data.pop("top_religions")
+
+print(china_data)
