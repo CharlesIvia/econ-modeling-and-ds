8515573acbd276acaad17b8b86e90e6d1a1140a9
Charles Ivia <iviacharles@gmail.com>
2020-09-28 15:53:21 +0300

    College education decision using npv

--- python_fundamentals/c_flow_exercises.py
+++ python_fundamentals/c_flow_exercises.py
@@ -37,5 +37,43 @@ if x < 0.5:
     print(f"{x} is less than 0.5")
 else:
     print(f"{x} is greater than 0.5")
-    
-    
+
+
+#Work-education decision
+
+# Discount rate
+r = 0.05
+
+# High school wage
+w_hs = 40000
+
+# College wage and cost of college
+c_college = 5000
+w_college = 50000
+
+
+def npv(paid, time, interest):
+    P0 = paid / ((1+interest)**time)
+    return P0
+
+# Compute npv of being a hs worker
+
+hs_npv = npv(w_hs, 40, r)
+print(hs_npv)
+
+# Compute npv of attending college
+
+college_cost_npv = npv(c_college, 4, r)
+print(college_cost_npv)
+
+# Compute npv of being a college worker
+
+c_npv = npv(w_college, 36, r)
+print(c_npv)
+
+# Is npv_collegeworker - npv_collegecost > npv_hsworker
+
+if c_npv - college_cost_npv > hs_npv:
+    print("The student should got to college")
+else:
+    print("Student not got to college")
