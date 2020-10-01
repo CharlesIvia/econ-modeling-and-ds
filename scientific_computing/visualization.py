1147fe72897f7834477336af2ca64a643e22f0fb
Charles Ivia <iviacharles@gmail.com>
2020-10-01 13:53:28 +0300

    Figures and axis

--- scientific_computing/visualization.py
+++ scientific_computing/visualization.py
@@ -20,4 +20,31 @@ y = np.sin(x)
 #step 3 - make a plot on our axis
 ax.plot(x, y)
 
+#difference btw figure and axis
+
+#axis is the canvas where we draw our plots
+#figure is the entire framed painting which incliudes axis itself
+
+fig, ax = plt.subplots()
+
+fig.set_facecolor("yellow")
+ax.set_facecolor("grey")
+
+
+# We specified the shape of the axes -- It means we will have two rows and three columns
+# of axes on our figure
+fig, axes = plt.subplots(2, 3)
+fig.set_facecolor("yellow")
+
+
+# Can choose hex colors
+colors = ["#065535", "#89ecda", "#ffd1dc", "#ff0000", "#6897bb", "#9400d3"]
+
+
+# axes is a numpy array and we want to iterate over a flat version of it
+for (ax, c) in zip(axes.flat, colors):
+    ax.set_facecolor(c)
+
+fig.tight_layout()
+
 plt.show()
