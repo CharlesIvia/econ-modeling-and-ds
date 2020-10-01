b0dddd697b5a54750822afc3f763e08def42719e
Charles Ivia <iviacharles@gmail.com>
2020-10-01 14:07:56 +0300

    Scatter and annotation

--- scientific_computing/visualization.py
+++ scientific_computing/visualization.py
@@ -68,4 +68,24 @@ ax[1].set_title("Land area (in millions miles squared)")
 fig.tight_layout()
 
 
+#SCATTER AND ANNOTATION
+
+N = 50
+
+np.random.seed(42)
+x = np.random.rand(N)
+y = np.random.rand(N)
+colors = np.random.rand(N)
+
+area = np.pi * (15 * np.random.rand(N))**2  # 0 to 15 point radii
+
+fig, ax = plt.subplots()
+ax.scatter(x, y, s=area, c=colors, alpha=0.5)
+
+ax.annotate(
+    "First point", xy=(x[0], y[0]), xycoords="data",
+    xytext=(25, -25), textcoords="offset points",
+    arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=0.6")
+)
+
 plt.show()
