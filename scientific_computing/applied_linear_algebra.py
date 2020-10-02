e8fc5d221cc2b3f9fc7a596ed9e6a60509b03073
Charles Ivia <iviacharles@gmail.com>
2020-10-02 13:09:04 +0300

    Element-wise operations

---
+++ scientific_computing/applied_linear_algebra.py
@@ -0,0 +1,17 @@
+# import numpy to prepare for code below
+import numpy as np
+import matplotlib.pyplot as plt
+
+# activate plot theme
+import qeds
+qeds.themes.mpl_style();
+
+#Elementwise operations
+
+x = np.array([1, 2, 3])
+y = np.array([4, 5, 6])
+
+print("Element-wise Addition", x + y)
+print("Element-wise Subtraction", x - y)
+print("Element-wise Multiplication", x * y)
+print("Element-wise Division", x / y)
