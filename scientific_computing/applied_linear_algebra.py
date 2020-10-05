dec5ea236e0224fcb4a4645508701a64edfa515b
Charles Ivia <iviacharles@gmail.com>
2020-10-05 12:23:16 +0300

    Element-wise and scalar operations on matrices

--- scientific_computing/applied_linear_algebra.py
+++ scientific_computing/applied_linear_algebra.py
@@ -26,3 +26,27 @@ print("Scalar Division", 3 / x)
 #Dot product
 
 print("Dot product with @ ", x@y)
+
+#MATRICES
+
+x = np.array([[1, 2, 3], [4, 5, 6]])
+print(x)
+
+y = np.ones((2, 3))
+print(y)
+
+z = np.array([[1, 2], [3, 4], [5, 6]])
+print(z)
+
+#Elelemnt-wise and scalar operations on matricess
+
+print("Element-wise Addition\n", x + y)
+print("Element-wise Subtraction\n", x - y)
+print("Element-wise Multiplication\n", x * y)
+print("Element-wise Division\n", x / y)
+
+
+print("Scalar Addition\n", 3 + x)
+print("Scalar Subtraction\n", 3 - x)
+print("Scalar Multiplication\n", 3 * x)
+print("Scalar Division\n", 3 / x)
