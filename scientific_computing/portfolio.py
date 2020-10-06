67a9616286628b56841b0c445974fb3bfa53d89d
Charles Ivia <iviacharles@gmail.com>
2020-10-06 12:39:01 +0300

    Computing NPV of a portfolio with an infinite lifetime

--- scientific_computing/portfolio.py
+++ scientific_computing/portfolio.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 #Static payoffs - As an example, consider a portfolio with 4 units of asset A, 2.5 units of asset B, and 8 units of asset C.
 
@@ -65,3 +66,37 @@ Vs = oil_fields @ discount
 print(Vs)
 
 print(f"The value of oilfields is {Vs.sum()}")
+
+
+#NPV for a portfolio with infinite lifetime
+
+#How different is this infinite horizon approximation from the T = 20
+
+# Depreciation of production rates
+gamma_A = 0.80
+gamma_B = 0.90
+
+# Interest rate discounting
+r = 0.05
+
+
+def infhor_NPV_oilfield(stating_output, gamma, r):
+    beta = gamma / (1+r)
+    return stating_output / (1 - beta)
+
+
+def compute_NPV_oilfield(starting_output, gamma, r, T):
+    outputs = starting_output * np.array([gamma**t for t in range(T)])
+    discount = np.array([(1 / (1+r))**t for t in range(T)])
+    npv = np.dot(outputs, discount)
+    return npv
+
+Ts = np.arange(2, 75)
+NPVs_A = np.array([compute_NPV_oilfield(5, gamma_A, r, t) for t in Ts])
+NPVs_B = np.array([compute_NPV_oilfield(2, gamma_B, r, t) for t in Ts])
+
+NPVs_T = NPVs_A + NPVs_B
+NPV_oo = infhor_NPV_oilfield(
+    5, gamma_A, r) + infhor_NPV_oilfield(2, gamma_B, r)
+print(NPV_oo)
+
