10d76bd76dd00bc54beb8a4904b5eab3742c99be
Charles Ivia <iviacharles@gmail.com>
2020-12-08 18:53:20 +0300

    DataFrame aggregations

--- pandas/pandas_functionality.py
+++ pandas/pandas_functionality.py
@@ -46,3 +46,47 @@ print(unemp.loc["01/01/2000", :])
 print(unemp.loc["01/01/2000":"06/01/2000", :])
 
 # DataFrame Aggregations
+
+# Built-in Aggregations
+
+print(unemp.mean())
+
+# Default aggregation is by column
+# Can use axis keyword argument to aggregate by row
+
+print(unemp.mean(axis=1).head())
+print(unemp.var(axis=1).head())
+
+# Writing Own Aggregations
+
+# Write a Python function that takes a Series as an input and outputs a single value.
+# Call the agg method with our new function as an argument.
+
+# Classifying employment status of states
+
+#
+# Step 1: We write the (aggregation) function that we'd like to use
+#
+
+
+def high_or_low(s):
+    """
+    This function takes a pandas Series object and returns high
+    if the mean is above 6.5 and low if the mean is below 6.5
+    """
+    if s.mean() < 6.5:
+        out = "Low"
+    else:
+        out = "High"
+    return out
+
+
+#
+# Step 2: Apply it via the agg method.
+#
+
+print(unemp.agg(high_or_low))
+
+# agg can take multiple fns at once
+
+print(unemp.agg([min, max, high_or_low]))
