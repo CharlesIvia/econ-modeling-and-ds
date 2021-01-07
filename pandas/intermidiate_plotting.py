05bc6eb2a134d4d974765bfc6bd89288229c2686
Charles Ivia <iviacharles@gmail.com>
2021-01-07 10:36:48 +0300

    Cleaning data part one

--- pandas/intermidiate_plotting.py
+++ pandas/intermidiate_plotting.py
@@ -6,6 +6,8 @@ import matplotlib
 import matplotlib.transforms as transforms
 import quandl
 import qeds
+from pandas.tseries.holiday import USFederalHolidayCalendar
+from pandas.tseries.offsets import CustomBusinessDay
 
 qeds.themes.mpl_style()
 
@@ -59,8 +61,10 @@ print(aapl.head())
 
 fig1, ax1 = plt.subplots()
 fig2, ax2 = plt.subplots()
+fig3, ax3 = plt.subplots()
 
 # plot the Adjusted open to account for stock split
+# Remember the independent variable goes into y axis and dependent variable into x axis
 
 ax1.plot(aapl["Adj. Open"])
 
@@ -77,5 +81,50 @@ ax1.set_yticks([0, 50, 100, 150, 200])
 # Other plots
 
 ax2.plot(aapl[["Adj. Low", "Adj. High"]])
+ax3.plot(aapl[["Low", "High"]])
 
-plt.show()
+# Data Cleaning
+
+bday_us = CustomBusinessDay(calendar=USFederalHolidayCalendar())
+
+
+def neigbor_dates(date, nbefore=3, nafter=3):
+    # Make sure the date is a datetime
+    date = pd.to_datetime(date)
+
+    # Create a list of business days
+
+    before_and_after = [date + i * bday_us for i in range(-nbefore, nafter + 1)]
+    return before_and_after
+
+
+dates = []
+
+for ann_date in announcement_dates.index:
+    dates.extend(neigbor_dates(ann_date))
+dates = pd.Series(dates)
+
+# Index into our DataFrrame using the new dates
+
+prices = aapl.loc[dates]
+print(prices.head())
+
+# Bring information on iPhone models into the DataFrame
+
+prices = prices.join(announcement_dates)
+print(prices["Model"].isnull().sum())
+print(prices["Model"].head(7))
+
+# Fill the NaNs below with Data
+
+prices = prices.ffill(limit=3)
+print(prices["Model"].isnull().sum())
+print(prices["Model"].head(7))
+
+# Fill NaNs above with Data
+
+
+prices = prices.bfill(limit=3)
+print(prices["Model"].isnull().sum())
+print(prices["Model"].head(7))
+# plt.show()
