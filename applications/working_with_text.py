c1f26e3090e50044fce2cac8cd052ddfe36cc1bb
Charles Ivia <iviacharles@gmail.com>
2021-02-02 17:28:37 +0300

    Initial avalanche incident data

--- applications/working_with_text.py
+++ applications/working_with_text.py
@@ -17,6 +17,44 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import qeds
+import json
+import os
+import urllib.request
+import time
+import requests
+import io
+import zipfile
+import warnings
 
 # activate plot theme
 qeds.themes.mpl_style()
+
+
+# Data
+
+# Data from from Avalanche Canada API
+# Rule of thumb: When considering scrapping a website check if the website has an API
+
+# Incident data
+
+url = "http://incidents.avalanche.ca/public/incidents/?format=json"
+
+req = urllib.request.Request(url)
+
+with urllib.request.urlopen(req) as response:
+    result = json.loads(response.read().decode("utf-8"))
+
+incident_list = result["results"]
+# print(incident_list)
+
+while result["next"] is not None:
+    req = urllib.request.Request(result["next"])
+    with urllib.request.urlopen(req) as response:
+        result = json.loads(response.read().decode("utf-8"))
+    incident_list = incident_list + result["results"]
+
+incident_brief = pd.DataFrame.from_dict(incident_list, orient="columns")
+pd.options.display.max_rows = 20
+pd.options.display.max_columns = 8
+
+print(incident_brief)
