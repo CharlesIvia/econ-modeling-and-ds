0dfc4fa264395d7a5d232bb079d0aa6fc88b2100
Charles Ivia <iviacharles@gmail.com>
2021-02-08 22:21:27 +0300

    Data prep

--- applications/heterogeneity.py
+++ applications/heterogeneity.py
@@ -16,3 +16,35 @@ import seaborn as sns
 url = "https://datascience.quantecon.org/assets/data/Triyana_2016_price_women_clean.csv.gz"
 df = pd.read_csv(url)
 print(df.describe())
+
+# data prep
+
+formula = """
+bw ~ pkh_kec_ever +
+  C(edu)*C(agecat) + log_xp_percap + hh_land + hh_home + C(dist) +
+  hh_phone + hh_rf_tile + hh_rf_shingle + hh_rf_fiber +
+  hh_wall_plaster + hh_wall_brick + hh_wall_wood + hh_wall_fiber +
+  hh_fl_tile + hh_fl_plaster + hh_fl_wood + hh_fl_dirt +
+  hh_water_pam + hh_water_mechwell + hh_water_well + hh_water_spring + hh_water_river +
+  hh_waterhome +
+  hh_toilet_own + hh_toilet_pub + hh_toilet_none +
+  hh_waste_tank + hh_waste_hole + hh_waste_river + hh_waste_field +
+  hh_kitchen +
+  hh_cook_wood + hh_cook_kerosene + hh_cook_gas +
+  tv + fridge + motorbike + car + goat + cow + horse
+"""
+bw, X = patsy.dmatrices(formula, df, return_type="dataframe")
+# some categories are empty after dropping rows will Null, drop now
+X = X.loc[:, X.sum() > 0]
+bw = bw.iloc[:, 0]
+treatment_variable = "pkh_kec_ever"
+treatment = X["pkh_kec_ever"]
+Xl = X.drop(["Intercept", "pkh_kec_ever", "C(dist)[T.313175]"], axis=1)
+# scale = bw.std()
+# center = bw.mean()
+loc_id = df.loc[X.index, "Location_ID"].astype("category")
+
+import re
+
+# remove [ ] from names for compatibility with xgboost
+Xl = Xl.rename(columns=lambda x: re.sub("\[|\]", "_", x))
