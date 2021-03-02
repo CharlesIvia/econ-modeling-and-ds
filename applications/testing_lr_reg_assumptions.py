4f35bb4c3df82908dfa6348fc77e26083b793169
Charles Ivia <iviacharles@gmail.com>
2021-03-02 14:31:57 +0300

    Linearlity assumption testing

--- applications/testing_lr_reg_assumptions.py
+++ applications/testing_lr_reg_assumptions.py
@@ -38,7 +38,7 @@ boston_model.fit(boston.data, boston.target)
 
 # Returning the R^2 for the model
 boston_r2 = boston_model.score(boston.data, boston.target)
-print("R^2: {0}".format(boston_r2))
+print(f"R^2: {boston_r2}")
 
 # Fitting the other model
 
@@ -48,7 +48,7 @@ linear_model.fit(linear_X, linear_y)
 
 # Returning the R^2 for the model
 linear_r2 = linear_model.score(linear_X, linear_y)
-print("R^2: {0}".format(linear_r2))
+print(f"R^2: {linear_r2}")
 
 
 def calculate_residuals(model, features, label):
@@ -60,3 +60,51 @@ def calculate_residuals(model, features, label):
     df_results["Residuals"] = abs(df_results["Actual"]) - abs(df_results["Predicted"])
 
     return df_results
+
+
+# Assumptions
+
+# First Assumption - Linearlity
+# This assumes that there is a linear relationship between the predictors
+# (e.g. independent variables or features)
+# and the response variable (e.g. dependent variable or label).
+# the predictors are additive.
+
+# How? use a scatter plot to see if residuals lie around a diagonal line on the scatter plot
+
+
+def linear_assumption(model, features, label):
+    """
+    Linearity: Assumes that there is a linear relationship between the predictors and
+               the response variable. If not, either a quadratic term or another
+               algorithm should be used.
+    """
+    print("Assumption 1: Linear Relationship between the Target and the Feature", "\n")
+
+    print(
+        "Checking with a scatter plot of actual vs. predicted.",
+        "Predictions should follow the diagonal line.",
+    )
+
+    # Calculating residuals for the plot
+    df_results = calculate_residuals(model, features, label)
+
+    # Plotting the actual vs predicted values
+    sns.lmplot(
+        x="Actual", y="Predicted", data=df_results, fit_reg=False, height=6, aspect=2
+    )
+
+    # Plotting the diagonal line
+    line_coords = np.arange(df_results.min().min(), df_results.max().max())
+    plt.plot(
+        line_coords, line_coords, color="darkorange", linestyle="--"  # X and y points
+    )
+    plt.title("Actual vs. Predicted")
+    plt.tight_layout()
+    plt.show()
+
+
+# Linearlity in the linear dataset
+
+linear_assumption(linear_model, linear_X, linear_y)
+linear_assumption(boston_model, boston.data, boston.target)
